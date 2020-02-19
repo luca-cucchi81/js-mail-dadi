08a35ab007af7ba08394a583af55698b6e8ec303
Luca <lu.cucchi@gmail.com>
2020-02-19 19:13:37 +0100

    Esercizio "dadi" finito (eventuali fix)

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -15,7 +15,17 @@ document.getElementById('secondo-nome').innerHTML = giocatoreDue;
 /*numeri dadi*/
 var dadiUno = Math.floor(Math.random() * 12) + 1;
 var dadiDue = Math.floor(Math.random() * 12) + 1;
-console.log(dadiUno);
 
 document.getElementById('lancio').innerHTML = dadiUno;
 document.getElementById('lancio2').innerHTML = dadiDue;
+
+/*condizionali*/
+if (dadiUno > dadiDue){
+    document.getElementById('vincitore').setAttribute('class', 'visible')
+    document.getElementById('nome-vincitore').innerHTML = giocatoreUno
+}else if (dadiUno < dadiDue){
+    document.getElementById('vincitore').setAttribute('class', 'visible')
+    document.getElementById('nome-vincitore').innerHTML = giocatoreDue
+}else{
+    document.getElementById('pareggio').setAttribute('class', 'visible')
+}
